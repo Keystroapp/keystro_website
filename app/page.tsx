9631773d70c578605610659cbb043d0d4ee89989
Ansh Rathod <anshrathod29@gmail.com>
2024-05-12 14:58:23 +0530

    added dmg file and updated page tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,17 @@ import CursorSection from "@/components/cursor-section";
 import KeystorkeSection from "@/components/keystorke-section";
 import { FloatingNav } from "@/components/navbar";
 import Pricing from "@/components/pricing";
+import SocialIcons from "@/components/social";
 import ThockSection from "@/components/thock-section";
 import { IconHome, IconMessage, IconMoneybag } from "@tabler/icons-react";
+import Link from "next/link";
 import WhyYouNeedThis from "../components/why-you-need-this";
-import SocialIcons from "@/components/social";
 
 export default function Home() {
-  const isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
+  const isMac =
+    typeof window !== "undefined"
+      ? navigator.platform.toUpperCase().indexOf("MAC") >= 0
+      : false;
   const navItems = [
     {
       name: "Home",
@@ -48,9 +52,12 @@ export default function Home() {
         <div className="flex justify-center mt-10 align-center  tablet:flex-col mobile:flex-col ">
           {isMac ? (
             <div className="px-6 flex justify-center mt-10 align-center  tablet:flex-col mobile:flex-col ">
-              <button className="shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0D5EF4] rounded-full text-white font-light transition duration-200 ease-linear">
-                Download for Mac
-              </button>
+              <Link href="keystro-macos.dmg">
+                <button className="shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0D5EF4] rounded-full text-white font-light transition duration-200 ease-linear">
+                  Download for Mac
+                </button>
+              </Link>
+
               <div className="mx-2 my-2"></div>
               <button className="  px-8 py-2  rounded-full text-white border-2 border-[#0D5EF4] font-light transition duration-200 ease-linear">
                 Download for Windows
