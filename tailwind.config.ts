e4da8302f9cacd1ba0d11cd2ea9221ca978caa07
Ansh Rathod <anshrathod29@gmail.com>
2024-05-14 11:50:34 +0530

    force dark mode only

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,8 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
+  darkMode: "class",
+
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
